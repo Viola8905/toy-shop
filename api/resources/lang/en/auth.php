7c3fd1ef9c26fc5a30bdfde9668af138439aa634
Viola8905 <viola89055@gmail.com>
2022-04-15 20:56:05 +0300

    deleted useless folder

--- api/resources/lang/en/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'failed' => 'These credentials do not match our records.',
-    'succeeded' => 'You have successfully authorized.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-];
